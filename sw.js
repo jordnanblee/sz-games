766e7d9c2477d1ababe245f534fee5e1dd86cd7f
jordnanblee <154616335+kaloodinaz@users.noreply.github.com>
2025-07-04 16:55:51 +0300

    Update

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
 const CACHE_NAME = 'cache-v2';
 const urlsToCache = [
-  '/index.html',
-  '/style.css',
-  '/manifest.json',
-  '/privacypolicy.html',
-  '/icons/icon-48x48.png',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png',
+ 'https://jordnanblee.github.io/sz-games/index.html',
+ 'https://jordnanblee.github.io/sz-games/style.css',
+ 'https://jordnanblee.github.io/sz-gamesmanifest.json',
+ 'https://jordnanblee.github.io/sz-games/privacypolicy.html',
+ 'https://jordnanblee.github.io/sz-gamesicons/icon-48x48.png',
+ 'https://jordnanblee.github.io/sz-games/icons/icon-72x72.png',
+ 'https://jordnanblee.github.io/sz-games/icons/icon-96x96.png',
+ 'https://jordnanblee.github.io/sz-games/icons/icon-128x128.png',
+ 'https://jordnanblee.github.io/sz-games/icons/icon-144x144.png',
+ 'https://jordnanblee.github.io/sz-games/icons/icon-152x152.png',
+ 'https://jordnanblee.github.io/sz-games/icons/icon-192x192.png',
+ 'https://jordnanblee.github.io/sz-games/icons/icon-384x384.png',
+ 'https://jordnanblee.github.io/sz-games/icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', event => {
